4091c6876d7e9bb6e507af28bb5ab1959a8476bb
a0905576360 <154112136+a0905576360@users.noreply.github.com>
2023-12-25 11:10:10 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,8 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
-
+let soundModelURL = 'https://a0905576360.github.io/fgfdg00/model.json';
 
 function preload() {
   // Load the model
